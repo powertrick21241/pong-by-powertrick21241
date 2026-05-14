d432946c70f2959faecde034bdf36f092a8ea989
Leandro <leandrocaschetto@gmail.com>
2026-05-14 11:01:06 +0200

    bot update

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from tkinter import font
-
 import pygame
 import random
-import numpy as np
+import time
 
 pygame.init()
 WIDTH = 800
@@ -33,8 +31,8 @@ imp=pygame.image.load("impostazioni.png")
 L_P_G_C_C_C= pygame.Rect(400, 300, 10, 10)
 speed = 1
 offset = (L_P_G_C_C_C.centery - player.centery) / (player.height / 2)
-score = 0
-scoret = 0
+score = 1
+scoret = 1
 scorev = pygame.font.SysFont("courier", 32, bold=True)
 scoretv = pygame.font.SysFont("courier", 32, bold=True)
 C_C_C = pygame.font.SysFont("courier", 20, bold=True)
@@ -51,15 +49,16 @@ B = 255
 P_E_C_C_C = "off"
 event = pygame.event.poll()
 mod = "banana" 
-WIDTH, HEIGHT = screen.get_size()
-fullscreen = False
 run = True
+racchettomm = pygame.transform.scale(racchettomm, (145, 145))
+racchettoll = pygame.transform.scale(racchettoll, (145, 145))
+pallopp = pygame.transform.scale(pallopp, (145, 145))
+clock = pygame.time.Clock()
 while run:
     key = pygame.key.get_pressed()
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
     screen.fill((0, 195, 0))
-
     if mod == "banana":
         pygame.draw.rect(screen, (0,195,0), buttomm)
         pygame.draw.rect(screen, (0,195,0), buttoll)
@@ -67,9 +66,6 @@ while run:
         screen.blit(racchettomm, (157,145))
         screen.blit(racchettoll, (467,145))
         screen.blit(pallopp, (305,325))
-        racchettomm = pygame.transform.scale(racchettomm, (145, 145))
-        racchettoll = pygame.transform.scale(racchettoll, (145, 145))
-        pallopp = pygame.transform.scale(pallopp, (145, 145))
         if buttomm.collidepoint(mouse) and click[0] and not prev_click:
             if playeron == "PLAYER":
                 playeron = "BOT"
@@ -95,13 +91,36 @@ while run:
                 player.move_ip(0, -1)
             if key[pygame.K_s] and player.y < HEIGHT - player.height:
                 player.move_ip(0, 1)
-        
+        elif playeron == "BOT":
+            if random.randint(1,100) <= 10 and (score <= scoret-10 and scoret > score*2):
+                occhio = L_P_G_C_C_C.centery + random.randint(-80,80)
+            else:
+                occhio = L_P_G_C_C_C.centery
+            Q_E_S = 4 if score == scoret or scoret//score <= 4 else scoret//score
+            V_B_O_P_M = random.randint(0,Q_E_S)
+            if V_B_O_P_M <= Q_E_S//2:
+                if occhio < player.centery and player.y > 0:
+                    player.move_ip(0, (-2 if V_B_O_P_M >= Q_E_S//2 else -1))
+                elif occhio > player.centery and player.y < HEIGHT - player.height:
+                    player.move_ip(0, (2 if V_B_O_P_M >= Q_E_S//2 else 1))
         if playerton == "PLAYER":
             if key[pygame.K_UP] and playert.y > 0:
                 playert.move_ip(0, -1)
             if key[pygame.K_DOWN] and playert.y < HEIGHT - playert.height:
                 playert.move_ip(0, 1)
-
+        elif playerton == "BOT":
+            if random.randint(1,100) <= 10 and (scoret <= score-10 and score > scoret*2):
+                occhio = L_P_G_C_C_C.centery + random.randint(-80,80)
+            else:
+                occhio = L_P_G_C_C_C.centery
+            Q_E_S = 4 if scoret == score or score//scoret <= 4 else score//scoret
+            V_B_O_P_M = random.randint(0,Q_E_S)
+            if L_P_G_C_C_C.x >= WIDTH // 2 - 200:
+                if V_B_O_P_M <= Q_E_S//2:
+                    if occhio < playert.centery and playert.y > 0:
+                        playert.move_ip(0, (-2 if V_B_O_P_M >= Q_E_S//2 else -1))
+                    elif occhio > playert.centery and playert.y < HEIGHT - playert.height:
+                        playert.move_ip(0, (2 if V_B_O_P_M >= Q_E_S//2 else 1))
         if L_P_G_C_C_C.colliderect(player):
             offset = (L_P_G_C_C_C.centery - player.centery) / (player.height / 2)
             vy = offset * speed
@@ -135,22 +154,22 @@ while run:
         pygame.draw.rect(screen, (255,0,0), player)
         pygame.draw.rect(screen, (0,0,255), playert)
         screen.blit(padella, (player.x, player.y))
-        padella = pygame.transform.scale(padella,(10, hamster))
         coy = player.y + player.height
         screen.blit(manico, (player.x-10, coy))
         screen.blit(padellat, (playert.x, playert.y))
-        padellat = pygame.transform.scale(padellat,(10, hamster))
         coyt = playert.y + playert.height
         screen.blit(maniaco, (playert.x, coyt))
+        padella = pygame.transform.scale(padella,(10, hamster))
+        padellat = pygame.transform.scale(padellat,(10, hamster))
         pygame.draw.rect(screen, (((R,G,B) if P_E_C_C_C == "on" else (255,255,0))), L_P_G_C_C_C)
-        score_text = scorev.render(str(score), False, (255, 255, 255))
-        scoret_text = scoretv.render(str(scoret), False, (255, 255, 255))
+        score_text = scorev.render(str(score-1), False, (255, 255, 255))
+        scoret_text = scoretv.render(str(scoret-1), False, (255, 255, 255))
         screen.blit(score_text, (250, 20))
         screen.blit(scoret_text, (500, 20))
         if hamster  >= 30:
             hamster -= 0.002
-        player.height = hamster
-        playert.height = hamster
+        player.height = int(hamster)
+        playert.height = int(hamster)
         L_P_G_C_C_C.x -= vx
         L_P_G_C_C_C.y += vy
     screen.blit(imp, (0,0))
@@ -166,13 +185,13 @@ while run:
         if pulsante.collidepoint(mouse) and click[0] and not prev_click:
             mod = "banana"
             hamster = 200
-            player.height = hamster
-            playert.height = hamster
+            player.height = int(hamster)
+            playert.height = int(hamster)
             player.y = 200
             playert.y = 200
             L_P_G_C_C_C.center = (WIDTH // 2, HEIGHT // 2)
-            score = 0
-            scoret = 0
+            score = 1
+            scoret = 1
         if riso.collidepoint(mouse) and click[0] and not prev_click:
             mod = prev_mod
         P_E_C_C_C = P_E_C_C_C
@@ -186,17 +205,11 @@ while run:
         screen.blit(resume_text, ((riso.x+riso.width), riso.y))
         screen.blit(leave_text, ((pulsante.x+pulsante.width), pulsante.y))
     prev_click= click[0]
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_F11:
-            fullscreen = not fullscreen
-            if fullscreen:
-                screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-            else:
-                screen = pygame.display.set_mode((800,600))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     
+    clock.tick(480)
     pygame.display.update()
 
 pygame.quit() #io sono un commento ma non un commento normale, sono un commento speciale che serve a far capire che il programma è finito, e che non c'è più niente da eseguire, quindi se tu sei un essere umano e stai leggendo questo commento, vuol dire che hai finito di leggere tutto il codice, e che adesso puoi chiudere questo programma, oppure puoi eseguirlo se vuoi, ma se lo esegui, ricordati di chiuderlo quando hai finito, altrimenti potrebbe causare problemi al tuo computer, quindi chiudi questo programma quando hai finito di usarlo, e ricorda di non eseguirlo se non sai cosa fa, perché potrebbe essere pericoloso per il tuo computer, quindi fai attenzione quando esegui questo programma, e ricorda di chiuderlo quando hai finito.
